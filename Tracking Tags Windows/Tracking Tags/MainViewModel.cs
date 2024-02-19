e19ae0a9bd164c46338cc652fbccc6eb2d5e6750
Andrew Forget <andrew.forget@andydragon.com>
2024-02-19 02:36:04 -0500

    Seed the WPF application

---
+++ Tracking Tags Windows/Tracking Tags/MainViewModel.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Input;
+using Newtonsoft.Json;
+using Notification.Wpf;
+
+using Tracking_Tags.Properties;
+
+namespace Tracking_Tags
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        private readonly HttpClient httpClient = new();
+        private readonly NotificationManager notificationManager = new();
+
+        public MainViewModel()
+        {
+            Tags = [];
+            Pages = [];
+            Version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "---";
+
+            _ = LoadPages();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string Version { get; set; }
+
+        #region Themes
+
+        private string themeName = "";
+
+        public string ThemeName
+        {
+            get
+            {
+                return themeName switch
+                {
+                    "SoftDark" => "Soft dark",
+                    "LightTheme" => "Light",
+                    "DeepDark" => "Deep dark",
+                    "DarkGreyTheme" => "Dark gray",
+                    "GreyTheme" => "Gray",
+                    _ => themeName,
+                };
+            }
+            set
+            {
+                if (themeName != value)
+                {
+                    themeName = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ThemeName)));
+                }
+            }
+        }
+
+        #endregion
+
+        #region User name
+
+        private string userName = string.Empty;
+        public string UserName 
+        {
+            get { return userName; } 
+            set 
+            {
+                if (userName != value)
+                {
+                    userName = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(UserName)));
+                    PopulateTags();
+                }
+            } 
+        }
+
+        #endregion
+
+        #region Page name
+
+        private string pageName = Settings.Default.Page ?? string.Empty;
+        public string PageName
+        {
+            get { return pageName; }
+            set
+            {
+                if (pageName != value)
+                {
+                    pageName = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PageName)));
+                    PopulateTags();
+                    Settings.Default.Page = value;
+                    Settings.Default.Save();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Include hash
+
+        private bool includeHash = Settings.Default.IncludeHash;
+        public bool IncludeHash
+        {
+            get { return includeHash; }
+            set
+            {
+                if (includeHash != value)
+                {
+                    includeHash = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IncludeHash)));
+                    PopulateTags();
+                    Settings.Default.IncludeHash = value;
+                    Settings.Default.Save();
+                }
+            }        
+        }
+
+        #endregion
+
+        public ObservableCollection<Tag> Tags { get; private set; }
+
+        private void PopulateTags()
+        {
+            Tags.Clear();
+            if (!string.IsNullOrEmpty(UserName) && !string.IsNullOrEmpty(PageName))
+            {
+                var prefix = IncludeHash ? "#" : "";
+                Tags.Add(new Tag($"{prefix}snap_{PageName}_{UserName}", notificationManager));
+                Tags.Add(new Tag($"{prefix}raw_{PageName}_{UserName}", notificationManager));
+                Tags.Add(new Tag($"{prefix}snap_featured_{UserName}", notificationManager));
+                Tags.Add(new Tag($"{prefix}raw_featured_{UserName}", notificationManager));
+            }
+        }
+
+        public ObservableCollection<string> Pages { get; private set; }
+
+        private async Task LoadPages()
+        {
+            try
+            {
+                // Disable client-side caching.
+                httpClient.DefaultRequestHeaders.CacheControl = new CacheControlHeaderValue
+                {
+                    NoCache = true
+                };
+                var pagesUri = new Uri("https://vero.andydragon.com/static/data/pages.json");
+                var content = await httpClient.GetStringAsync(pagesUri);
+                if (!string.IsNullOrEmpty(content))
+                {
+                    var pagesCatalog = JsonConvert.DeserializeObject<PagesCatalog>(content) ?? new PagesCatalog();
+                    foreach (var page in pagesCatalog.Pages.Select(page => page.Name))
+                    {
+                        Pages.Add(page);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // TODO andydragon : handle errors
+                Console.WriteLine("Error occurred: {0}", ex.Message);
+            }
+        }
+    }
+
+    internal class Tag(string text, NotificationManager notificationManager)
+    {
+        public string Text { get; set; } = text;
+
+        private ICommand? copyCommand = null;
+
+        public ICommand Copy
+        {
+            get
+            {
+                return copyCommand ??= new CommandHandler(
+                    () => 
+                    {
+                        Clipboard.SetText(Text);
+                        notificationManager.Show(
+                            "Copied",
+                            "Copied the tag to the clipboard",
+                            type: NotificationType.Success,
+                            areaName: "WindowArea",
+                            expirationTime: TimeSpan.FromSeconds(1.5));
+                    }, 
+                    () => !string.IsNullOrEmpty(Text));
+            }
+        }
+    }
+
+    public class CommandHandler(Action action, Func<bool> canExecute) : ICommand
+    {
+        private readonly Action action = action;
+        private readonly Func<bool> canExecute = canExecute;
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute();
+        }
+
+        public void Execute(object? parameter)
+        {
+            action();
+        }
+    }
+
+    public class PagesCatalog
+    {
+        public PagesCatalog()
+        {
+            Pages = [];
+        }
+
+        public PageEntry[] Pages { get; set; }
+    }
+
+    public class PageEntry
+    {
+        public PageEntry()
+        {
+            Name = string.Empty;
+        }
+
+        public string Name { get; set; }
+        public string? PageName { get; set; }
+    }
+}
---
+++ Tracking Tags Windows/Tracking Tags/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using FramePFX.Themes;
+using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tracking_Tags.Properties;
+
+namespace Tracking_Tags
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var theme = Settings.Default.Theme;
+            switch (theme)
+            {
+                case "SoftDark": ThemesController.SetTheme(ThemeType.SoftDark); break;
+                case "LightTheme": ThemesController.SetTheme(ThemeType.LightTheme); break;
+                case "DeepDark": ThemesController.SetTheme(ThemeType.DeepDark); break;
+                case "DarkGreyTheme": ThemesController.SetTheme(ThemeType.DarkGreyTheme); break;
+                case "GreyTheme": ThemesController.SetTheme(ThemeType.GreyTheme); break;
+                default:
+                    {
+                        if (IsLightTheme())
+                        {
+                            ThemesController.SetTheme(ThemeType.LightTheme);
+                        }
+                        break;
+                    }
+            }
+
+            if (DataContext is MainViewModel viewModel)
+            {
+                viewModel.ThemeName = ThemesController.CurrentTheme.GetName();
+            }
+        }
+
+        private static bool IsLightTheme()
+        {
+            using var key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize");
+            var value = key?.GetValue("AppsUseLightTheme");
+            return value is int i && i > 0;
+        }
+
+        private void OnThemeClick(object sender, RoutedEventArgs e)
+        {
+            switch (ThemesController.CurrentTheme.GetName())
+            {
+                case "SoftDark": ThemesController.SetTheme(ThemeType.LightTheme); break;
+                case "LightTheme": ThemesController.SetTheme(ThemeType.DeepDark); break;
+                case "DeepDark": ThemesController.SetTheme(ThemeType.DarkGreyTheme); break;
+                case "DarkGreyTheme": ThemesController.SetTheme(ThemeType.GreyTheme); break;
+                case "GreyTheme": ThemesController.SetTheme(ThemeType.SoftDark); break;
+            }
+
+            Settings.Default.Theme = ThemesController.CurrentTheme.GetName();
+            Settings.Default.Save();
+
+            if (DataContext is MainViewModel viewModel)
+            {
+                viewModel.ThemeName = ThemesController.CurrentTheme.GetName();
+            }
+        }
+
+        private void OnPaste(object sender, RoutedEventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                if (DataContext is MainViewModel viewModel)
+                {
+                    viewModel.UserName = Clipboard.GetText();
+                }
+            }    
+        }
+    }
+}
