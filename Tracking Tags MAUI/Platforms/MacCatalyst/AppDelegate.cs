1bcd88dcc9136f1653580430a41e170f5ed91b2d
Andrew Forget <andrew.forget@andydragon.com>
2024-02-27 08:44:42 -0500

    Purge MAUI app

--- Tracking Tags MAUI/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Tracking_Tags_MAUI;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
